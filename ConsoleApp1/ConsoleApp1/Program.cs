6b942f746a890e8a3daf8cefa392a1138d646303
asmaa497 <sososalma33333@gmail.com>
2022-05-22 21:25:29 +0200

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello ");
+            Console.WriteLine("Hello 2");
 
         }
     }
